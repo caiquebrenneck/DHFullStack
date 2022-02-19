ffc169ddb721b8e0cd054930792509d5c882793a
caiquebrenneck <caique.rodrigues99@hotmail.com>
2022-02-18 23:48:46 -0300

    aula 7 completa

--- src/aulas/TesteEmpregados.java
+++ src/aulas/TesteEmpregados.java
@@ -1,10 +1,30 @@
 package Aulas;
 
 public class TesteEmpregados {
-	
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Empregados[] lista = new Empregados[3];
 		
-		Empregados[] lista = new Empregados();
+		lista[0] = new Empregados("Ana Kélvia", 10000);
+		lista[1] = new Empregados("Lucas Theberge", 11000);
+		lista[2] = new Empregados("Adrielle Barros", 15000);
+		
+		for(Empregados passar:lista)
+		{
+			passar.imprimir();
+		}
+		
+		System.out.println("***************");
+		
+		for(Empregados passar:lista)
+		{
+			passar.aumentarSalario(10);
+			passar.imprimir();
+		}
+		
 		
 	}
+
 }

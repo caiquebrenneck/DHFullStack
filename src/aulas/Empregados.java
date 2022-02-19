9c99b699588fbb91c878321feb851fc42eaa26af
caiquebrenneck <caique.rodrigues99@hotmail.com>
2022-02-18 23:43:59 -0300

    aula 7 incompleta

--- src/aulas/Empregados.java
+++ src/aulas/Empregados.java
@@ -1,5 +1,56 @@
 package Aulas;
 
+import java.text.NumberFormat;
+
 public class Empregados {
 
+	// Criacao dos atributos
+	
+	private String nome;
+	private double salario;
+
+	public Empregados(String nome, double salario) {
+		this.setNome(nome);
+		this.setSalario(salario);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public void aumentarSalario(double percentual) {
+		salario *= 1 + percentual / 100;
+	}
+	
+	public String formatarMoeda() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(); // Coloque a moeda do país
+		nf.setMinimumFractionDigits(2); // Indica a quantidade de casa decimal 
+		String formatoMoeda = nf.format(salario); // formata um valor para um formtação monetária 
+		return formatoMoeda;
+	}
+	
+	public void imprimir() {
+		System.out.println(nome + "\t\t" + "Salário: " + this.formatarMoeda());
+	}
+	
 }
+
+
+
+
+
+
+
+
--- src/aulas/TesteEmpregados.java
+++ src/aulas/TesteEmpregados.java
@@ -1,5 +1,10 @@
 package Aulas;
 
 public class TesteEmpregados {
-
+	
+	public static void main(String[] args) {
+		
+		Empregados[] lista = new Empregados();
+		
+	}
 }

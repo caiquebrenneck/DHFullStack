0b844b83198b3e24c17d054ed65d2e9efac199eb
caiquebrenneck <caique.rodrigues99@hotmail.com>
2022-02-21 21:16:51 -0300

    oitava aula

---
+++ src/aulas/TesteEmpregado1.java
@@ -0,0 +1,13 @@
+package Aulas;
+
+public class TesteEmpregado1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Empregado1 emp = new Empregado1("Edu", "Rua da Marola 386", "54670334556", 9484844, 22, 123, 20000, 15);
+		emp.imprimirInfo();
+		emp.calcularSalario();
+	}
+
+}

ff824e6c71273c713b3624e5f75ab49328510496
caiquebrenneck <caique.rodrigues99@hotmail.com>
2022-03-11 18:10:01 -0300

    Decima e terceira aula

---
+++ src/aulas/Colecao2.java
@@ -0,0 +1,10 @@
+package Aulas;
+
+public class Colecao2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/aulas/Colecoes1.java
@@ -0,0 +1,10 @@
+package Aulas;
+
+public class Colecoes1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

a4a5e4d1fcf0c365caf40cdaaa9c63af1bc72ef4
caiquebrenneck <caique.rodrigues99@hotmail.com>
2022-03-11 21:11:00 -0300

    14º aula

--- src/aulas/Colecao2.java
+++ src/aulas/Colecao2.java
@@ -1,10 +1,93 @@
 package Aulas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Colecao2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		int op;
+		Scanner ler = new Scanner(System.in);
+		
+		ArrayList<String> estoque = new ArrayList();
+		
+		do {
+			System.out.println("\n---------------");
+			System.out.println("\n(1) Deseja adicionar produtos no estoque? ");
+			System.out.println("\n(2) Deseja remover produtos do estoque? ");
+			System.out.println("\n(3) Deseja atualzar produtos do estoque? ");
+			System.out.println("\n(4) Deseja mostrar todos os produtos do estoque? ");
+			System.out.println("\n(0) Deseja sair do programa? ");
+			System.out.println("\n---------------");
+			System.out.println("\nDigite uma opção");
+			op = ler.nextInt();
+			
+			switch(op) {
 
+			case 1:
+				ler.nextLine();
+				System.out.println("\nDigite o produto para adicionar no estoque: ");
+				String produto = ler.nextLine();
+				estoque.add(produto);
+			System.out.println(estoque);
+			break;
+			
+			case 2:
+				ler.nextLine();
+				System.out.println("\nDigite o produto para remover do estoque: ");
+				String remover = ler.nextLine();
+				if(estoque.contains(remover)) {
+					estoque.remove(remover);
+					System.out.println("\nO produto: " + remover + ", foi removido do estoque.");
+					
+				} else {
+					System.out.println("\nProduto não existe no estoque");
+				}
+				System.out.println(estoque);
+			break;
+			
+			case 3:
+				ler.nextLine();
+				System.out.println("\nDigite o produto que deseja atualiar: ");
+				String verifica = ler.nextLine();
+				System.out.println("\nDigite o nome do produto  que entrará no lugar do " + verifica + " : ");
+				String novo = ler.nextLine();
+				
+				if(estoque.contains(verifica)) {
+					estoque.remove(verifica);
+					estoque.add(novo);
+				} else {
+					System.out.println("\nProduto não existe no estoque");
+				}
+				
+				System.out.println(estoque);
+				break;
+				
+			case 4:
+				System.out.println("\nOs produtos do estoque são: ");
+				System.out.println(estoque);
+				break;
+				default:
+					System.out.println("\nOpção inválida.");
+			}
+		} while(op != 0);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/aulas/Excecao1.java
@@ -0,0 +1,26 @@
+package Aulas;
+
+public class Excecao1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String frase = null; 
+		String novaFrase = null;
+		
+		try {
+			novaFrase = frase.toUpperCase();
+		}
+		catch(NullPointerException e) // captura da possivel excecao
+		{
+			System.out.println("\nA frase inicial está nula, para solucionar tal problema, "
+					+ "foi lhe atribuido um valor default...");
+			frase = "Agora tem algo aqui";
+			novaFrase = frase.toUpperCase();
+		}
+		System.out.println("\nFrase antia: " + frase);
+		System.out.println("\nNova frase: " + novaFrase);
+		
+	}
+
+}
